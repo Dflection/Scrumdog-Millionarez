12e9e4462088a697d4c7c4c57d40329506611ba6
Josh4066 <midnight7079@gmail.com>
2025-02-05 23:39:44 -0800

    Update Changing_Gui.py
    Updated gui

--- Changing_Gui.py
+++ Changing_Gui.py
@@ -6,21 +6,22 @@ import random
 import scrumdog_queue
 import Student_Class
 import Database3
+from random import randint
 
 class ScrumGui:
     # Creates Gui
     def __init__(self, window, on_submit=None):
         self.window = window
         self.window.title("Columbia College Sign Statistics Calculator")
-        self.window.geometry("1000x1000")
+        self.window.geometry("700x700")
         self.window.resizable(False, False)
         # External callback for processing data, help from AI with this
         self.on_submit = on_submit
 
         # Create and resize background photo
         directory_path = os.path.dirname(__file__)
-        original_img = Image.open(os.path.join(directory_path, "mkweenie.png"))
-        resized_image = original_img.resize((520, 250))
+        original_img = Image.open(os.path.join(directory_path, "red2.png"))
+        resized_image = original_img.resize((700, 700))
         self.new_image = ImageTk.PhotoImage(resized_image)
         self.background_label = tk.Label(window, image=self.new_image)
         self.background_label.place(relwidth=1, relheight=1)
@@ -82,7 +83,10 @@ class ScrumGui:
     def submit(self):
         try:
             # Collect data from the entry fields
-            num_students = int(self.student_entry.get())
+            if int(self.student_entry.get()) < 51:
+                num_students = int(self.student_entry.get())
+            else:
+                num_students = randint(20,49)
             # speed_of_cars = float(self.keyword2_entry.get())
             sign_display_time = float(self.sign_entry.get())
             num_signs = int(self.sign_time_entry.get())
@@ -108,40 +112,45 @@ class ScrumGui:
 
             # Get the averages of time each sign was seen
             averages1 = db.averages(1)  # You can change the number here if you want averages for a number of days
+            percentage1 = db.percentages(1)
 
              # Get the averages of time each sign was seen
             averages2 = db.averages(2)  # You can change the number here if you want averages for a number of days
+            percentage2 = db.percentages(2)
 
              # Get the averages of time each sign was seen
             averages3 = db.averages(3)  # You can change the number here if you want averages for a number of days
+            percentage3 = db.percentages(3)
 
              # Get the averages of time each sign was seen
             averages4 = db.averages(4)  # You can change the number here if you want averages for a number of days
+            percentage4 = db.percentages(4)
 
              # Get the averages of time each sign was seen
             averages5 = db.averages(5)  # You can change the number here if you want averages for a number of days
+            percentage5 = db.percentages(5)
 
             # Prepare the averages output
-            averages_output1 = "Average Time Each Sign Was Seen for 1 day students (seconds):\n"
+            averages_output1 = f"{percentage1}% of the signs were seen by 1 day students.\nThe Average time each student saw each sign:\n"
             for avg in averages1:
                 averages_output1 += f"Sign {avg['Sign']}: {avg['Average_Seconds_Seen']} sec\n"
             
-            averages_output2 = "Average Time Each Sign Was Seen for 2 day students (seconds):\n"
+            averages_output2 = f"{percentage2}% of the signs were seen by 2 day students.\nThe Average time each student saw each sign:\n"
             for avg in averages2:
                 averages_output2 += f"Sign {avg['Sign']}: {avg['Average_Seconds_Seen']} sec\n"
 
                      
-            averages_output3 = "Average Time Each Sign Was Seen for 3 day students (seconds):\n"
+            averages_output3 = f"{percentage3}% of the signs were seen by 3 day students.\nThe Average time each student saw each sign:\n"
             for avg in averages3:
                 averages_output3 += f"Sign {avg['Sign']}: {avg['Average_Seconds_Seen']} sec\n"
 
                      
-            averages_output4 = "Average Time Each Sign Was Seen for 4 day students (seconds):\n"
+            averages_output4 = f"{percentage4}% of the signs were seen by 4 day students.\nThe Average time each student saw each sign:\n"
             for avg in averages4:
                 averages_output4 += f"Sign {avg['Sign']}: {avg['Average_Seconds_Seen']} sec\n"
 
                      
-            averages_output5 = "Average Time Each Sign Was Seen for 5 day students (seconds):\n"
+            averages_output5 = f"{percentage5}% of the signs were seen by 5 day students.\nThe Average time each student saw each sign:\n"
             for avg in averages5:
                 averages_output5 += f"Sign {avg['Sign']}: {avg['Average_Seconds_Seen']} sec\n"
 
@@ -173,69 +182,6 @@ class ScrumGui:
 
 
 
-
-
-
-#     def submit(self):
-#         # Clear the results text box
-#         self.results.delete(1.0, tk.END)
-
-#         # Get values from the entry boxes
-#         student_value = self.student_entry.get()
-#         car_speed_value = self.car_speed_entry.get()
-#         sign_time_value = self.sign_entry.get()
-#         sign_count_value = self.sign_time_entry.get()
-
-#         # Gather values for validation
-#         values = [student_value, car_speed_value, sign_time_value, sign_count_value]
-
-#         # Make sure that all boxes are filled and values are only positive integers.
-#         if (self.validator.validate_not_empty(values) and 
-#             self.validator.validate_integer(student_value) and 
-#             self.validator.validate_integer(car_speed_value) and 
-#             self.validator.validate_integer(sign_time_value) and 
-#             self.validator.validate_integer(sign_count_value)):
-
-#             # Throw values into a dictionary.
-#             data = {
-#                 "Number of Students": student_value,
-#                 "Car Speed": car_speed_value,
-#                 "Time of Sign Display": sign_time_value,
-#                 "# of Signs to Display": sign_count_value,
-#             }
-            
-#             # Call the external on_submit callback with the data and a reference to this GUI instance. Got help from AI on this
-#             if self.on_submit:
-#                 self.on_submit(data, self)
-#             else:
-#                 self.display_message("Running Calculations...")
-
-#     def display_message(self, message):
-#         """
-#         Clears the results text widget and displays the provided message.
-
-#         Parameters:
-#             message (str): The message to display in the results text widget.
-#         """
-#         self.results.delete(1.0, tk.END)
-#         self.results.insert(tk.END, message)
-
-# class UserValidation:
-#     def validate_not_empty(self, values):
-#         # Check if all of the entry boxes are filled
-#         for value in values:
-#             if not value.strip():
-#                 messagebox.showerror("Invalid Input", "All Entry Boxes must be filled.")
-#                 return False
-#         return True
-
-#     def validate_integer(self, value):
-#         # Check to make sure entries are only positive integers
-#         if value.isdigit() and int(value) > 0:
-#             return True
-#         else:
-#             messagebox.showerror("Invalid Input", "Please only enter positive whole numbers.")
-#             return False
 
 # If this module is run as a script, launch the GUI.
 if __name__ == "__main__":
